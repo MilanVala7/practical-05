670059da936964fe16400f2a7e89efc7ec3b0c82
Milan Vala <valamilan2005@gmail.com>
2026-03-20 17:41:53 +0530

    Completed Practical 5

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿namespace ExceptionHandling;
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        for (int i = 0; i <= 5; i++)
+        {
+            try
+            {
+                Console.WriteLine($"Index {i}: {numbers[i]}");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("This is finally block");
+            }
+        }
+    }
+}
